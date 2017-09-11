325c4da69763739bfea5525e693c8401213e6c5b
Aneta Baloyan <arambaloyan@Arams-MacBook-Pro.local>
2017-09-11 20:28:48 +0400

    first commit

---
+++ Homework2/homework2.js
@@ -0,0 +1,9 @@
+const alert='This is not an official webpage!';
+const year=2000;
+const name='Anet';
+console.log(name+' is '+ (2017-year) +' years old.')
+console.log("She's sooo small, but she'll learn js soon! ;) ")
+window.alert(alert+ "\n"+ name+' created this page.');
+
+
+
